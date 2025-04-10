ab8f5529e4a0439e8fb0743c601be0509462cb64
ParthM02 <parthmehta02@yahoo.com>
2025-04-10 14:55:01 -0500

    too ez

--- grind.c
+++ grind.c
@@ -71,8 +71,10 @@ static bool print_and_free_list(struct grind_elem* head) {
     }
     while (itr != NULL) {
         printf("Uh Oh! %ld Bytes Leaked From Line: %d\n", itr->size, itr->index);
+        struct grind_elem* temp = itr->next;
         free_grind_elem(&itr);
-        itr = itr->next;
+
+        itr = temp;
     }
     return true;
 }
